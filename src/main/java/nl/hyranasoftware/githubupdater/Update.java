7a8b719add671d0fd007596cf8de3d9afed97d5d
Danny ter Haar <dannyterhaar@msn.com>
2017-04-24 18:44:22 +0200

    Initialized all the domain classes

--- src/main/java/nl/hyranasoftware/githubupdater/Update.java
+++ src/main/java/nl/hyranasoftware/githubupdater/Update.java
@@ -5,13 +5,25 @@
  */
 package nl.hyranasoftware.githubupdater;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import nl.hyranasoftware.githubupdater.domain.Release;
 
 /**
  *
  * @author danny_000
  */
 public class Update {
+    
+    /*
+    https://developer.github.com/v3/repos/releases/#get-the-latest-release
+    */
     
     private String repoOwner;
     private String repoName;
@@ -25,6 +37,20 @@ public class Update {
     
     public boolean checkForUpdates(){
         return true;
+    }
+    
+    public Release getLatestRelease() throws UnirestException{
+        String requestUrl = "https://api.github.com/repos/eternia16/javaGMR/releases/latest";
+        String response = Unirest.get(requestUrl).asJson().getBody().toString();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JodaModule());
+        try {
+            Release release = mapper.readValue(response, Release.class);
+            return release;
+        } catch (IOException ex) {
+            Logger.getLogger(Update.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
     public File downloadFile(){
--- src/main/java/nl/hyranasoftware/githubupdater/domain/Release.java
+++ src/main/java/nl/hyranasoftware/githubupdater/domain/Release.java
@@ -5,10 +5,107 @@
  */
 package nl.hyranasoftware.githubupdater.domain;
 
+import java.util.List;
+import org.joda.time.DateTime;
+
 /**
  *
  * @author danny_000
  */
 public class Release {
+    
+    String url;
+    String html_url;
+    String assets_url;
+    String upload_url;
+    String tarball_url;
+    String zipball_url;
+    int id;
+    String tag_name;
+    String target_commitish;
+    String name;
+    String body;
+    boolean draft;
+    boolean prerelease;
+    DateTime created_at;
+    DateTime published_at;
+    GithubUser author;
+    List<Asset> assets;
+
+    public Release() {
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getHtml_url() {
+        return html_url;
+    }
+
+    public String getAssets_url() {
+        return assets_url;
+    }
+
+    public String getUpload_url() {
+        return upload_url;
+    }
+
+    public String getTarball_url() {
+        return tarball_url;
+    }
+
+    public String getZipball_url() {
+        return zipball_url;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTag_name() {
+        return tag_name;
+    }
+
+    public String getTarget_commitish() {
+        return target_commitish;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public boolean isDraft() {
+        return draft;
+    }
+
+    public boolean isPrerelease() {
+        return prerelease;
+    }
+
+    public DateTime getCreated_at() {
+        return created_at;
+    }
+
+    public DateTime getPublished_at() {
+        return published_at;
+    }
+
+    public GithubUser getAuthor() {
+        return author;
+    }
+
+    public List<Asset> getAssets() {
+        return assets;
+    }
+
+
+    
+    
+    
     
 }
